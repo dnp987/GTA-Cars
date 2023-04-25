c74ce1b201ef8c993d5cee308d82758dd825d32f
dnp987 <dnp987@gmail.com>
2023-04-25 18:15:57 -0400

    On going updates

--- src/Cars/Chrysler/Car_data_DowntownChrysler.py
+++ src/Cars/Chrysler/Car_data_DowntownChrysler.py
@@ -9,8 +9,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from Quotes.Excel_utils2 import Excel_utils2
-from Cars.CreateDealerSheet2 import CreateDealerSheet
-from Cars.mazda_fix import mazda_fix
+#from Cars.mazda_fix import mazda_fix
 from Cars.browser_start import browser_start
 from Cars.close_out import close_out
 
@@ -25,63 +24,55 @@ if __name__ == '__main__':
     data_out = Excel_utils2(' ', date_time, 'out') # set the spreadsheet tab to the dealer name
     
     headless = False
-    #headless = True
+    #headless = True # this site seemingly won't work with headless mode
     driver = browser_start(url, headless)
         
     wait = WebDriverWait(driver, 10) # set the default web element wait time
-    WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.logo_wrapper'))) # site logo
+    #WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.logo_wrapper'))) # site logo
+    WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'a.title_wrapper'))) # site logo
+    
     print (driver.title)
-    num_cars = driver.find_element(By.CSS_SELECTOR, 'span.srp_carbon_results__count--digits')
-    print ("Number of cars found on site: " , num_cars.text)
-    scroll_pause_time = 4 
-    page_count = 0
-    
-    while True:
-        try:
-            driver.find_element(By.CSS_SELECTOR, '.listing-used-button-loading').click() # get the next set of data
-            page_count +=1
-            sleep(6) # need at least 4-6 seconds pause for the page data to load and settle.
-            try:
-                #driver.find_element(By.CSS_SELECTOR, '.cherry-popper-15096-close').click() # close the annoying pop-up if it appears
-                driver.find_element(By.CSS_SELECTOR, '.popup-image__closer').click() # close the annoying pop-up if it appears
-            except:
-                continue
-        except:
-            print (page_count, " pages in total")
-            break
-
-    href = (driver.page_source).split('href')
-    
-    car_desc = driver.find_elements(By.CSS_SELECTOR, '.car-name') # car description, make, model
-    car_prices = driver.find_elements(By.CSS_SELECTOR, '.price') # car prices
-    stock = driver.find_elements(By.CSS_SELECTOR, 'div.car-info > div:nth-child(1)') # car stock #
-    details_links = driver.find_elements(By.CSS_SELECTOR, '.car-image')
-     
-    count = 0
+    num_cars = int(driver.find_element(By.CSS_SELECTOR, 'span.srp_carbon_results__count--digits').text)
+    print ("Number of cars found on site: " , num_cars)
+    pages_remaining = True
+        
     zero = 0
+    count = 0
     car_info = []
-    for index, car in enumerate(car_desc):
-        car_name = (car.text +" ").split() # convert to a list
-        year = car_name[0].split() # convert the year to a list
-        make = car_name[1] 
-        model = car_name[2:] # model is already a list
-        make, model = mazda_fix(make, model)
-        make = make.split() # convert to a list
-        model = [' '.join(model)] # merge the model into one list element
-        car_desc = year + make + model
-        price = car_prices[index].text
-        price = re.sub("[^0-9]", "", price) #remove text and keep the numeric part
-        if len(price) == 0: # if the price is "Call for price" or something non-numeric, set the price to 0
-            price = '0'
-            zero += 1
-        price = price.split() # convert to a list
-        stock_num = stock[index].text
-        stock_num = (re.sub("[#]", "", stock_num)).split() # remove "#" from the stock number and convert to a list
-        #link = (details_links[index].get_attribute('src')).split()
-        link = ('https://www.downtownchrysler.ca/en/used-inventory').split() # set to the dealer's site since the url's can't be easily found
-        print (index,":", car_desc, price, stock_num, link)
-        car_info.append(dealer_id + car_desc + price + stock_num + link) 
-        count += 1
+    while pages_remaining:
+        car_desc = driver.find_elements(By.CSS_SELECTOR, 'h2.vehicle_title') # car description, make, model
+        car_prices = driver.find_elements(By.CSS_SELECTOR, 'dd.vehicle_price') # car prices
+        details_links = driver.find_elements(By.CSS_SELECTOR, 'a.vehicle_item__vehicle_link')
+        
+        for index, (cars, prices, links) in enumerate(zip(car_desc, car_prices, details_links)):
+            car = cars.text.replace('Pre-Owned ', '') # remove Pre-Owned from the description
+            year = car[0:4].split()
+            make_model = car[5:].split()
+            make = make_model[0].split()
+            model = make_model[1:]
+            model = [' '.join(make_model[1:])] # merge the model into one list element
+            car_desc = year + make + model
+            
+            price = prices.text
+            price = re.sub("[^0-9]", "", price) #remove text and keep the numeric part
+            if len(price) == 0: # if the price is "Call for price" or something non-numeric, set the price to 0
+                price = '0'
+                zero += 1
+                
+            price = price.split() # convert to a list
+            stock_num = ('N/A').split() # stock number is found on each car details page
+            link = (links.get_attribute('href')).split()
+            count += 1
+            print (index, " : ", dealer_id + car_desc + price + stock_num + link)
+            car_info.append(dealer_id + car_desc + price + stock_num + link)                                                     
+        try:
+            next_page = driver.find_element(By.CSS_SELECTOR, "li.next")
+            next_page.click() # click on Next link
+            WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'a.title_wrapper'))) # site logo
+            #sleep (2)
+            print ('Next page loaded: ',  driver.current_url)
+        except:
+            pages_remaining = False
+            print ('*** Last page ***')
     
     close_out(driver, dealer, count, zero, num_cars, data_out, file_out, date_time, car_info)
-    
